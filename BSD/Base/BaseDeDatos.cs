dcd1a557c60d2c120894fae9725442374700c433
ESTUDIANTE <ESTUDIANTE@PC14_LAB1>
2025-07-12 11:35:43 -0500

    Agregar archivos de proyecto.

---
+++ BSD/Base/BaseDeDatos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using BSD.Clases;
+
+namespace BSD.BaseDatos
+{
+    public static class BaseDeDatos
+    {
+        public static List<Cliente> BaseDatosCliente = new List<Cliente>();
+
+        public static void imprimirCliente()
+        {
+            foreach (Cliente cliente in BaseDatosCliente)
+            {
+                cliente.imprimir();
+            }
+        }
+    }
+}
---
+++ BSD/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BSD.Clases;
+
+namespace BSD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cedula del cliente: ");
+            string cedula = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Ingrese los nombres del cliente: ");
+            string nombres = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Ingrese los apellidos del cliente: ");
+            string apellidos = Console.ReadLine();
+            Console.WriteLine();
+
+
+            Cliente objCliente = new Cliente(cedula, nombres, apellidos);
+            BaseDatos.BaseDeDatos.imprimirCliente();
+            Console.ReadLine();
+
+            Console.WriteLine("Ingrese la cedula del cliente: ");
+            cedula = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Ingrese los nombres del cliente: ");
+            nombres = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Ingrese los apellidos del cliente: ");
+            apellidos = Console.ReadLine();
+            Console.WriteLine();
+
+
+            Cliente objClient2 = new Cliente(cedula, nombres, apellidos);
+            BaseDatos.BaseDeDatos.imprimirCliente();
+            Console.ReadLine();
+        }
+    }
+}
